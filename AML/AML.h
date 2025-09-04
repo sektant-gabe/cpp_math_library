541978cb4e4e979e909faf1d40819215e8e82a90
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-09-04 20:20:08 -0300

    created project, vector3, matrix33 and related tests

---
+++ AML/AML.h
@@ -0,0 +1,7 @@
+#ifndef AML_H
+#define AML_H
+
+#include "AMLVector3.h"
+#include "AMLMatrix33.h"
+
+#endif// AML_H
---
+++ example/main.cpp
@@ -0,0 +1,14 @@
+// System Includes
+#include <cmath>
+#include <iostream>
+#include <string>
+
+// Program Includes
+#include "AML.h"
+
+using namespace AML;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
